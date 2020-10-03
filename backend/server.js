a8e68f0caf536035e824ed4163f1136f8ad49ae6
n4dz <nadzamini@gmail.com>
2020-10-03 16:18:13 -0400

    exercises 1 to 3 done

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { createUser } = require('./handlers');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { createUser } = require("./handlers");
+const { getUser } = require("./handlers");
 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./client/build'))
+  .use(morgan("tiny"))
+  .use(express.static("./client/build"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
-  .post('/users', createUser)
+  .post("/users", createUser)
+  .get("/users", getUser)
 
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,9 +1,73 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase";
+import "firebase/auth";
+
+require("dotenv").config();
+
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
 export const AppContext = createContext(null);
 
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+const AppProvider = ({ children, signInWithGoogle, signOut, user }) => {
+  const [appUser, setAppUser] = useState({});
+  const [message, setMessage] = useState("");
+
+  const handleSignOut = () => {
+    signOut();
+    setAppUser({});
+  };
+
+  useEffect(() => {
+    // if (user) console.log(user);
+    if (user) {
+      fetch(`/users`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setAppUser(json.data);
+          setMessage(json.message);
+        });
+    }
+  }, [user]);
+  return (
+    // <AppContext.Provider value={{ appUser, signInWithGoogle, handleSignOut }}>
+    <AppContext.Provider
+      value={{ appUser, signInWithGoogle, handleSignOut, message }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-export default AppProvider;
+// export default AppProvider;
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+  firebaseApp,
+})(AppProvider);
